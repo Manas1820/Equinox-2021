58d3b9e403e97cccc7854ce45be235084368d9c0
GeethKuldeep <geethkuldeep@gmail.com>
2021-02-21 20:46:27 +0530

    Integrated auth

--- lib/screens/email_sigin_page.dart
+++ lib/screens/email_sigin_page.dart
@@ -9,6 +9,7 @@ class EmailSigninPage extends StatelessWidget {
     const color = const Color(0xff0E1C36);
     var color1 = const Color(0xffFBD00D);
     return Scaffold(
+      resizeToAvoidBottomInset: true,
         resizeToAvoidBottomPadding: false,
         body: SingleChildScrollView(
           child: Padding(
@@ -16,7 +17,7 @@ class EmailSigninPage extends StatelessWidget {
             child: Column(
               children: [
 
-                Image(image: AssetImage('images/Equinox Logo-02 4.png'),
+                Image(image: AssetImage('images/logo.png'),
                   width: MediaQuery.of(context).size.width*0.7,
                   height: MediaQuery.of(context).size.width*0.7,
                 ),
