5652da3ffcc104b9c383ea0b411ca855fd987f55
apoorvsingh2000 <apoorvsinghlko@gmail.com>
2021-02-26 23:38:38 +0530

    🎪 team page ui fixes

--- lib/screens/team_page.dart
+++ lib/screens/team_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equinox_21/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -52,136 +53,130 @@ class _TeamState extends State<Team> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            body: loading
-                ? Container(
-                    child: Padding(
-                      padding: const EdgeInsets.all(25.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      body: SafeArea(
+        child: loading
+            ? Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: screenWidth(context) * 0.05),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: screenHeight(context) * 0.05,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          SizedBox(
-                            height: 45,
+                          Text('\tMy Team',
+                              style: headingTextStyle(context, isDarkMode)),
+                          FlatButton(
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            color: Color(0xffE1D342),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            onPressed: () async {
+                              final _firebaseAuth = FirebaseAuth.instance;
+                              await _firebaseAuth.signOut();
+                            },
                           ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 45,
+                      ),
+                      Text(
+                        "We're called",
+                        style: teamPageSubheadingStyle(context, isDarkMode),
+                      ),
+                      SizedBox(
+                        height: 13,
+                      ),
+                      Text(
+                        "Bahubali",
+                        style: showOfflineSubheadingText(context, isDarkMode),
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Text(
+                        "Our Stars",
+                        style: teamPageSubheadingStyle(context, isDarkMode),
+                      ),
+                      Flexible(
+                          child: ListView.builder(
+                              itemCount: data['teammate'].length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Padding(
+                                  padding: textTeamPagePadding(context),
+                                  child: Text(
+                                    data['teammate'][index].toString(),
+                                    style: teamPageTextStyle(context, isDarkMode),
+                                  ),
+                                );
+                              })),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 35.0, right: 35.0, top: 45.0),
+                        child: Divider(
+                          color: Color(0xffE1D342),
+                          thickness: 4,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18,
+                      ),
+                      Column(
+                        children: [
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text('\tMy Team',
-                                  style: headingTextStyle(context, isDarkMode)),
-                              FlatButton(
-                                child: Text(
-                                  "Logout",
-                                  style: TextStyle(fontSize: 20.0),
+                              Center(
+                                child: FlatButton(
+                                  child: Image.asset(
+                                    'images/discord.png',
+                                    height: 58,
+                                    width: 58,
+                                  ),
+                                  onPressed: () {
+                                    launch('https://discord.com/invite/EfV4kK3Bgu');
+                                  },
                                 ),
-                                color: Color(0xffE1D342),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15)),
-                                onPressed: () async {
-                                  final _firebaseAuth = FirebaseAuth.instance;
-                                  await _firebaseAuth.signOut();
-                                },
                               ),
                             ],
                           ),
-                          SizedBox(
-                            height: 45,
-                          ),
-                          Text(
-                            "We're called",
-                            style: teamPageSubheadingStyle(context, isDarkMode),
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                          Text(
-                            "Bahubali",
-                            style: showOfflineSubheadingText(context, isDarkMode),
-                          ),
-                          SizedBox(
-                            height: 35,
-                          ),
-                          Text(
-                            "Our Stars",
-                            style: teamPageSubheadingStyle(context, isDarkMode),
-                          ),
-                          Flexible(
-                              child: ListView.builder(
-                                  itemCount: data['teammate'].length,
-                                  itemBuilder: (BuildContext context, int index) {
-                                    return Padding(
-                                      padding: textTeamPagePadding(context),
-                                      child: Text(
-                                        data['teammate'][index].toString(),
-                                        style:
-                                            teamPageTextStyle(context, isDarkMode),
-                                      ),
-                                    );
-                                  })),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 35.0, right: 35.0, top: 45.0),
-                            child: Divider(
-                              color: Color(0xffE1D342),
-                              thickness: 4,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 18,
-                          ),
-                          Column(
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Row(
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Center(
-                                    child: FlatButton(
-                                      child: Image.asset(
-                                        'images/discord.png',
-                                        height: 58,
-                                        width: 58,
-                                      ),
-                                      onPressed: () {
-                                        launch(
-                                            'https://discord.com/invite/EfV4kK3Bgu');
-                                      },
-                                    ),
+                                  Text(
+                                    'Discord Channel',
+                                    style: GoogleFonts.raleway(
+                                        color: Color(0xffE1D342),
+                                        fontSize: screenWidth(context) * 0.06,
+                                        fontWeight: FontWeight.bold),
                                   ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                  SizedBox(height: 15),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
                                       Text(
-                                        'Discord Channel',
+                                        'code:',
+                                        style: GoogleFonts.raleway(
+                                            color: isDarkMode
+                                                ? textColorNight
+                                                : textColorDay,
+                                            fontSize: screenWidth(context) * 0.045),
+                                      ),
+                                      Text(
+                                        '\t\tXs12Vd',
                                         style: GoogleFonts.raleway(
                                             color: Color(0xffE1D342),
-                                            fontSize: screenWidth(context) * 0.06,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      SizedBox(height: 15),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            'code:',
-                                            style: GoogleFonts.raleway(
-                                                color: isDarkMode
-                                                    ? textColorNight
-                                                    : textColorDay,
-                                                fontSize:
-                                                    screenWidth(context) * 0.045),
-                                          ),
-                                          Text(
-                                            '\t\tXs12Vd',
-                                            style: GoogleFonts.raleway(
-                                                color: Color(0xffE1D342),
-                                                fontSize:
-                                                    screenWidth(context) * 0.045),
-                                          ),
-                                        ],
+                                            fontSize: screenWidth(context) * 0.045),
                                       ),
                                     ],
                                   ),
@@ -189,19 +184,26 @@ class _TeamState extends State<Team> {
                               ),
                             ],
                           ),
-                          SizedBox(height: 35),
                         ],
                       ),
-                    ),
-                    decoration: BoxDecoration(
-                      gradient: isDarkMode
-                          ? darkBackgroundGradient
-                          : lightBackgroundGradient,
-                    ))
-                : Container(
-                    child: Center(
-                        child: CircularProgressIndicator(
-                            backgroundColor: Color(0xffFFED43))),
-                  )));
+                      SizedBox(height: 35),
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  gradient:
+                      isDarkMode ? darkBackgroundGradient : lightBackgroundGradient,
+                ))
+            : Container(
+                decoration: BoxDecoration(
+                  gradient:
+                      isDarkMode ? darkBackgroundGradient : lightBackgroundGradient,
+                ),
+                child: Center(
+                    child: CircularProgressIndicator(
+                        backgroundColor: Color(0xffFFED43))),
+              ),
+      ),
+    );
   }
 }
